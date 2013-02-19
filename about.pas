f597711bd6ca0dd9f752d476cdbc4244431b625b
Maciek <maciej.zakrzewski87@gmail.cm>
2013-02-19 19:52:18 +0100

    about

---
+++ about.pas
@@ -0,0 +1,42 @@
+unit about;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, ExtCtrls,
+  StdCtrls;
+
+type
+
+  { Tabout }
+
+  Tabout = class(TForm)
+    Button1: TButton;
+    Image1: TImage;
+    Label1: TLabel;
+    Label2: TLabel;
+    procedure FormCreate(Sender: TObject);
+  private
+    { private declarations }
+  public
+    { public declarations }
+  end;
+
+var
+  aboutWindow: Tabout;
+
+implementation
+
+{$R *.lfm}
+
+{ Tabout }
+
+procedure Tabout.FormCreate(Sender: TObject);
+begin
+  self.hide;
+end;
+
+end.
+
